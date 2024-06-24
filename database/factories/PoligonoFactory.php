ca9e125375d046e56472c45f0188fd9339d86cd3
Paultool <paultool@gmail.com>
2024-06-24 11:15:19 -0600

    factory de poligonos con centro en laguna

--- database/factories/PoligonoFactory.php
+++ database/factories/PoligonoFactory.php
@@ -12,11 +12,29 @@ class PoligonoFactory extends Factory
 
     public function definition()
     {
+        // Definimos los límites del área de la laguna
+        $minLat = 19.776;
+        $maxLat = 19.798;
+        $minLng = -99.147;
+        $maxLng = -99.127;
+        $polygonSize = 0.001;
+
+        // Generar un centro de coordenadas dentro de los límites
+        $centerLat = $this->faker->randomFloat(6, $minLat, $maxLat);
+        $centerLng = $this->faker->randomFloat(6, $minLng, $maxLng);
+
+        // Generar las coordenadas del polígono basado en el centro
+        $coordinates = [
+            ['lat' => $centerLat - $polygonSize / 2, 'lng' => $centerLng - $polygonSize / 2],
+            ['lat' => $centerLat - $polygonSize / 2, 'lng' => $centerLng + $polygonSize / 2],
+            ['lat' => $centerLat + $polygonSize / 2, 'lng' => $centerLng + $polygonSize / 2],
+            ['lat' => $centerLat + $polygonSize / 2, 'lng' => $centerLng - $polygonSize / 2],
+            ['lat' => $centerLat - $polygonSize / 2, 'lng' => $centerLng - $polygonSize / 2], // Cerrar el polígono
+        ];
+
         return [
             'nombre' => $this->faker->word,
-            'coordenadas' => json_encode([
-                ['lat' => $this->faker->latitude, 'lng' => $this->faker->longitude],             
-            ]),
+            'coordenadas' => json_encode($coordinates),
             'escena_id' => Escena::factory(),
             'descripcion' => $this->faker->sentence,
         ];
