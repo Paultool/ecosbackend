5040a8acccab2557367d0c6ba43dd6d23ff439dc
Paultool <paultool@gmail.com>
2024-06-23 18:58:36 -0600

    add vue

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hola desde Vue.js</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
